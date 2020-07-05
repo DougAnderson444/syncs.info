df065622095f3b6693499c4c7a602bf5f5158177
DougA <douganderson444@gmail.com>
2020-07-05 16:25:12 -0400

    wait for it

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,54 +1,65 @@
 <script>
-
-import ServiceWorker from '../components/serviceWorker'
+  import ServiceWorker from '../components/serviceWorker'
+  export let nodeId
 
 </script>
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  figure {
+    margin: 0 0 1em 0;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  p {
+    margin: 1em auto;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Sapper project template</title>
 </svelte:head>
 
 <h1>Great success!</h1>
 
 <figure>
-	<img alt='OK' src='ok.png'>
-	<figcaption>Have fun with Sapper!</figcaption>
+  {#if !nodeId}
+    <img alt="Wait" src="wait-for-it.png" />
+    <figcaption>Loading... wait for it... aaaaand,</figcaption>
+  {:else}
+    <img alt="OK" src="ok.png" />
+    <figcaption>Have fun with Sapper!</figcaption>
+  {/if}
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>
+  <strong>
+    Try editing this file (src/routes/index.svelte) to test live reloading.
+  </strong>
+</p>
 
-<ServiceWorker />
+<ServiceWorker bind:nodeId  />
